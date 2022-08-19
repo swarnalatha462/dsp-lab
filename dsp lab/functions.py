1f385427e1b366eaa32fa4c81e725abdace72a85
swarnalatha462 <111379191+swarnalatha462@users.noreply.github.com>
2022-08-20 00:01:54 +0530

    Delete dsp lab directory

--- dsp lab/functions.py
+++
@@ -1,8 +0,0 @@
-def even_odd(x):
-	if(x%2==0):
-		print("even number")
-	else:
-		print("odd number")
-
-n=int(input("enter a number: "))
-even_odd(n)
--- dsp lab/sum_of_n_numbers.py
+++
@@ -1,7 +0,0 @@
-
-sum=0
-n=int(input("Enter a numnber :"))
-for i in range(0,n+1):
-	sum+=i
-print(sum)
-
